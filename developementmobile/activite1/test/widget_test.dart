a79e82a46d8cbf27731846bf2234daaa7f7743af
IvinoDev <kayantao@protonmail.com>
2025-10-27 20:18:35 +0000

    code done

--- developementmobile/activite1/test/widget_test.dart
+++ developementmobile/activite1/test/widget_test.dart
@@ -11,20 +11,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:activite1/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Magazine app smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MonAppli());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our app bar title is displayed.
+    expect(find.text('Magazine Infos'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify that the floating action button is present.
+    expect(find.text('Click'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that menu and search icons are present.
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.byIcon(Icons.search), findsOneWidget);
   });
 }
